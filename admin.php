c00abad17488ed8d06d2a2f87bfd3cff3023f68b
Unknown <feyza@keles.nl>
2019-04-16 11:27:12 +0200

    add scheme file

--- admin.php
+++ admin.php
@@ -1,8 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 16-Apr-19
- * Time: 09:27
- */
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fifa</title>
+</head>
+<body>
+<div class="index">
+    <div class="description">
+        <a href="team.php"></a>
+        
+    </div>
+    <div>
+        <a href="scheme.php"></a>
+    </div>
+</div>
+</body>
+</html>
