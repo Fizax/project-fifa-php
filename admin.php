60898f52e805e66b65ff62d8c98614f846859bf2
Unknown <feyza@keles.nl>
2019-04-16 10:08:49 +0200

    index + frontand

---
+++ admin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 16-Apr-19
+ * Time: 09:27
+ */
---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Fifa</title>
+</head>
+<body>
+    <h1>Plan je Fifa!</h1>
+    <div class="index">
+        <div class="description">
+            <form action="loginController.php" method="post">
+                <input type="hidden" name="type" value="login">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">wachtwoord</label>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <div class="button">
+                    <input type="submit" value="login">
+                </div>
+
+            </form>
+        </div>
+        <div>
+            <form action="loginController.php" method="post">
+                <input type="hidden" name="type" value="register">
+                <div class="form-group">
+                    <label for="username">username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">email</label>
+                    <input type="email" name="email" id="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">password</label>
+                    <input type="password" name="password" id="password" >
+                </div>
+
+                <div class="form-group">
+                    <label for="password1">Please confirm your password</label>
+                    <input type="password" name="password1" id="password1" required>
+                </div>
+
+
+                <label class="container">
+                    <input type="checkbox" name="boxname" value='itsChecked' >
+                    <span class="checkmark">akoord met algemene voorwaarde</span>
+
+                </label>
+
+                <div class="button">
+                    <input type="submit" value="Register" name="button">
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
