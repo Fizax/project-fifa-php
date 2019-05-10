742bf0c4d28353b3f66d6e70093c403082f3cb21
lorenzo rijnvos <lorenzo.rijnvos@gmail.com>
2019-05-10 12:29:28 +0200

    delete klaar
    je kan nu teams deleten van je database

--- delete_controler.php
+++ delete_controler.php
@@ -5,8 +5,11 @@
  * Date: 10/05/2019
  * Time: 11:27
  */
+require 'config.php';
 $id=$_GET['id'];
-$sql ="DELETE row FROM teams WHERE id= $id";
-$query = mysqli_query($connect, $sql);
-$row = mysqli_fetch_array($query);
+$sql = "DELETE FROM `teams` WHERE `id` = $id;";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':id' => $id
+]);
 header("Location: admin.php");
